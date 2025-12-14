83101d90c64365eafb45947a29d04a62374e1d25
ajinkyaparte530-max <ajinkyaparte530@gmail.com>
2025-12-14 22:21:46 +0530

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
+import streamlit as st
+import numpy as np
+
+st.title("🧮 Calculator")
+
+num1 = st.number_input("Enter first number", value=0.0)
+num2 = st.number_input("Enter second number", value=0.0)
+
+operation = st.selectbox(
+    "Choose Operation",
+    ["Add", "Subtract", "Multiply", "Divide"]
+)
+
+if st.button("Calculate"):
+    if operation == "Add":
+        st.success(num1 + num2)
+
+    elif operation == "Subtract":
+        st.success(num1 - num2)
+
+    elif operation == "Multiply":
+        st.success(num1 * num2)
+
+    elif operation == "Divide":
+        if num2 == 0:
+            st.error("Cannot divide by zero")
+        else:
+            st.success(num1 / num2)
 
